07891d2f5197943833e0211f20d760929a5169ee
Adrijan Rogan <adrijanrogan@protonmail.com>
2022-02-08 14:59:09 +0100

    Update theme

--- app/src/main/java/com/rogandev/lpp/ui/theme/Color.kt
+++ app/src/main/java/com/rogandev/lpp/ui/theme/Color.kt
@@ -4,8 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val Green = Color(0xFF1E7B4B)
 val GreenVariant = Color(0XFF96D4B4)
-
 val BackgroundLight = Color(0xFFFEFEFE)
-val BackgroundDark = Color(0xFF111111)
-
-val Error = Color(0xFFE3682F)
--- app/src/main/java/com/rogandev/lpp/ui/theme/Theme.kt
+++ app/src/main/java/com/rogandev/lpp/ui/theme/Theme.kt
@@ -1,11 +1,15 @@
 package com.rogandev.lpp.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-private val LightColorPalette = lightColors(
+val ColorPalette = lightColors(
     primary = Green,
     primaryVariant = GreenVariant,
     secondary = Green,
@@ -18,10 +22,18 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
 )
 
+val Typography = Typography()
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(5.dp),
+    medium = RoundedCornerShape(10.dp),
+    large = RoundedCornerShape(20.dp)
+)
+
 @Composable
 fun EmonaTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = LightColorPalette,
+        colors = ColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
