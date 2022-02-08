2e7233992768a7178ce29180655ff07c2d932e5f
Adrijan Rogan <adrijanrogan@protonmail.com>
2022-02-08 08:43:12 +0100

    Use only light theme

--- app/src/main/java/com/rogandev/lpp/ui/theme/Theme.kt
+++ app/src/main/java/com/rogandev/lpp/ui/theme/Theme.kt
@@ -1,24 +1,9 @@
 package com.rogandev.lpp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-
-private val DarkColorPalette = darkColors(
-    primary = Green,
-    primaryVariant = GreenVariant,
-    secondary = Green,
-    secondaryVariant = GreenVariant,
-    background = BackgroundDark,
-    surface = BackgroundDark,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onBackground = Color.White,
-    onSurface = Color.White,
-)
 
 private val LightColorPalette = lightColors(
     primary = Green,
@@ -27,22 +12,16 @@ private val LightColorPalette = lightColors(
     secondaryVariant = GreenVariant,
     background = BackgroundLight,
     surface = Color.White,
-    onPrimary = Color.Black,
-    onSecondary = Color.Black,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
     onBackground = Color.Black,
     onSurface = Color.Black,
 )
 
 @Composable
-fun EmonaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun EmonaTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
